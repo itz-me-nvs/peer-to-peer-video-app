cff6ee7546dcc153f737f39778b523e81a15b380
navas <muhammednavas320203@gmail.com>
2025-05-17 23:05:45 +0530

    changes

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
-const socket = io('ws://localhost:5000', {
-  transports: ['websocket'], // enforce WebSocket only - import.meta.env.VITE_SOCKET_URL
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ['websocket'], // enforce WebSocket only
   secure: true
 });
 export default socket;
